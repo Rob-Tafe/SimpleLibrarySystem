e480e8564b848c00470f0cf2e27cfffa085cfd74
Rob-Tafe <P214430@tafe.wa.edu.au>
2025-09-15 15:22:53 +0800

    Staged

--- Form1.cs
+++ Form1.cs
@@ -30,120 +30,19 @@ namespace SimpleLibrarySystem
     } // End of Form1 class
 
 
-
-    // Attributes for the LibraryMembers class
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class memberIDAttribute : Attribute
-    {
-        string memberID = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class memberNameAttribute : Attribute
-    {
-        string memberName = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class memberEmailAttribute : Attribute
-    {
-        string memberEmail = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class memberActiveAttribute : Attribute
-    {
-        Boolean memberActive;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class memberHasBooksCheckedOutAttribute : Attribute
-    {
-        // List memberHasTheseBooksCheckedOut;
-    }
-    // End of Attributes for the LibraryMembers class
-
-    [memberID, memberName, memberEmail, memberActive, memberHasBooksCheckedOut]
     public class LibraryMembers
     {
 
 
-
     }
 
 
-
-    // Attributes for the Books class
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookIDAttribute : Attribute
-    {
-        int bookID;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookTitleAttribute : Attribute
-    {
-        string bookTitle = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookAuthorAttribute : Attribute
-    {
-        string bookAuthor = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookPublicationDateAttribute : Attribute
-    {
-        string bookPublicationDate = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookISBNAttribute : Attribute
-    {
-        string bookISBN = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookNumberOfCopiesAttribute : Attribute
-    {
-        int bookNumberOfCopies;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookNumberOfAvailableCopiesAttribute : Attribute
-    {
-        int bookNumberOfAvailableCopies;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookCopyTrackerAttribute : Attribute
-    {
-        // List bookCopyTracker;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookCheckedOutDateAttribute : Attribute
-    {
-        string bookCheckedOutDate = string.Empty;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class bookDueDateAttribute : Attribute
-    {
-        string bookDueDate = string.Empty;
-    }
-    // End of Attributes for the Books class
-
-    [bookID, bookTitle, bookAuthor, bookPublicationDate, bookISBN, bookNumberOfCopies, 
-        bookNumberOfAvailableCopies, bookCopyTracker, bookCheckedOutDate, bookDueDate]
     public class Books
     {
 
 
 
     }
-
 
 
     // Attributes for the BookOperations class
@@ -157,41 +56,12 @@ namespace SimpleLibrarySystem
     }
 
 
-
-    // Attributes for the LibraryDataTracking class
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class allBooksListAttribute : Attribute
-    {
-        // List allBooksList;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class allBooksBorrowedListAttribute : Attribute
-    {
-        // List allBooksBorrowed;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class allMembersListAttribute : Attribute
-    {
-        // List allMembersList;
-    }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class allMembersThatHaveBooksBorrowedListAttribute : Attribute
-    {
-        // List allMembersThatHaveBooksBorrowed;
-    }
-    // End of Attributes for the LibraryDataTracking class
-
-    [allBooksList, allBooksBorrowedList, allMembersList, allMembersThatHaveBooksBorrowedList]
     public class LibraryDataTracking
     {
         
 
 
     }
-
 
 
 } // End of SimpleLibrarySystem namespace
